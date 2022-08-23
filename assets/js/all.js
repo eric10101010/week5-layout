08835951c8f5dbcb3b2d583a435d737d128ca0d9
Eric <erickaivalue@gmail.com>
2022-08-23 23:44:49 +0800

    Update 2022-08-23T15:44:47.714Z

---
+++ assets/js/all.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+$(document).ready(function () {
+  $('.hamburger').click(function () {
+    $(this).toggleClass('open');
+  });
+});
+//# sourceMappingURL=all.js.map
